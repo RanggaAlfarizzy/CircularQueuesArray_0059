898a0aaf3058999e98430f5abe3dbdf8cba9dc6d
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-06-12 17:53:17 +0800

    Mengatur tanda untuk antrian yang kosong

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -3,10 +3,17 @@ using namespace std;
 
 class Queues
 {
-    private:
+private:
     static const int max = 5;
     int FRONT, REAR;
     int queue_array[5];
 
+public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
 
-}
+
+};
