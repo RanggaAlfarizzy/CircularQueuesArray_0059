c810610029814e0bce9e4584a2278fe875712d42
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-06-12 17:55:26 +0800

    mengecek apakah antriannya penuh

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -22,6 +22,13 @@ public:
         cin >> num;
         cout << endl;
 
+        //1. cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max -1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n"; // 1.a
+            return;                       // 2.b
+        }
+
 
     }
 
