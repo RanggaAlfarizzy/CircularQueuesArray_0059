be728a8435129659b43282ebc80066deb8117034
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-06-12 18:20:38 +0800

    Menunjukan elemen yang dihapus yang mana

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -54,6 +54,9 @@ public:
             cout << "Queue underflow\n";
             return;
         }
+        cout << "\nThe Element deleted from the queue is: " << queue_array
+        [FRONT] << "\n";
+        
 
     }
 
